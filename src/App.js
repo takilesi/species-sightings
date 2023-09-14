1401fa8f70b7c6b5a6e9ba82d1b372fb386e6d1b
Doug <124453486+takilesi@users.noreply.github.com>
2023-09-14 15:46:46 -0400

    change wording

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App({viewBirdArray}) {
   console.log("App.js", viewBirdArray)
   return (
     <div className="App">
-       <p className="header">Enter a comma separated list into input box. Click Add to see new results.</p>
+       <p className="header">Enter a comma separated list into input box. Click Add to see new results. (Numbers in data set represent individual species sightings)</p>
        <h1 className="title">Calculate Species Sightings</h1>
        <Birds />
        <footer className="footer">Project Inception: <a href="https://www.hackerrank.com/challenges/migratory-birds/problem?isFullScreen=true">HackerRank Migratory-Birds Problem</a></footer>
