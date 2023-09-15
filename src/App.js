4f61a7b5abdce0a0ee88350cf8876a9b6f260d65
Doug <124453486+takilesi@users.noreply.github.com>
2023-09-15 11:57:05 -0400

    more css tweaks for mobile deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App({viewBirdArray}) {
   return (
     <div className="App">
        <p className="header">(Numbers in data set represent individual species sightings)</p>
-       <h1 className="title">Calculate Species Sightings</h1>
+       <p className="title">Calculate Species Sightings</p>
        <Birds />
        <footer className="footer">Inception: <a href="https://www.hackerrank.com/challenges/migratory-birds/problem?isFullScreen=true">HackerRank Migratory-Birds</a></footer>
     </div>
